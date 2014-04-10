4d7180f72c8d5df95ea04db0106964251677285c
Allen3 <allen.3.ysj@gmail.com>
2014-04-10 17:04:07 +0800

    Just homework done! Satisfied with it. :) , README updated.

--- Contact.cpp
+++ Contact.cpp
@@ -27,8 +27,9 @@ struct _info{
 }__attribute__((packed));  //_info
 
 /*********************VARIABLE DEFINES*************************/
-_info *List = NULL;
-int ListLength = 0;
+_info *List = NULL , *Tail = NULL;
+int  ListLength = 0;
+int  command;
 
 /************************PROTOTYPES****************************/
 void Print_WelcomeInfo();
@@ -45,6 +46,10 @@ void Delete_Contact_bytelnum();
 int  getQueryChoice();
 void Query_Contact_byname();
 void Query_Contact_bytelnum();
+int  getModifyChoice();
+void Modify_Contact_name(int );
+void Modify_Contact_telnum(int );
+void Modify_Contact_birthday(int );
 
 /*********************FUNCTIONS DEFINES************************/
 void Print_WelcomeInfo(){
@@ -55,13 +60,14 @@ void Print_WelcomeInfo(){
 
 void Print_CommandInfo(){
     printf("\n\n\n");
-    printf("============================================================================\n");
+    printf("====================================Main Menu=========================================\n");
     printf("\tHere are the commands you can type in (just type in the entry number)\n\n");
     printf("\t0. List all contacts.\n");
     printf("\t1. Create a new contact.\n");
     printf("\t2. Delete a contact.\n");
     printf("\t3. Query detail information.\n");
-    printf("\t4. Exit this Program.\n");
+    printf("\t4. Modify a contact's information.\n");
+    printf("\t5. Exit this Program.\n");
     printf("\n");
     printf("Command: ");
     //Append new funtcions
@@ -86,26 +92,34 @@ void Add_NewContact(){
     scanf("%s" , newNode->name);
     printf("\tTelephone: ");
     scanf("%s" , newNode->telnum);
-    printf("\tBirthday(**** ** **): ");
-    scanf("%d%d%d" , &newNode->birthday.year , &newNode->birthday.month , &newNode->birthday.day);
+    printf("\tBirthday(**** ** ** or just type in 0 to skip this): ");
+    int option;
+    scanf("%d" , &option);
+    if (option != 0){
+        scanf("%d%d" , &newNode->birthday.month , &newNode->birthday.day);
+    }   //option != 0
     
     if (List){
-        newNode->next = List;
-        List->last = newNode;
-        List = newNode;
+        newNode->next = NULL;
+        newNode->last = Tail;
+        Tail->next = newNode;
+        Tail = newNode;
     }
     else{
         newNode->last = NULL;
         newNode->next = NULL;
         List = newNode;
+        Tail = newNode;
     }
 }   //Add_NewContact
 
 int getListChoice(){
     int command;
-    printf("\t\tHere are the commands you can type in (just type in the entry number)\n");
+    printf("--------------------------------------------------------------------------------\n");
+    printf("\t\tHere are the commands you can type in (just type in the entry number)\n\n");
     printf("\t\t1. List all contacts information on the screen.\n");
     printf("\t\t2. Output all contacts information in the file \"ContactList.txt\".\n");
+    printf("\t\t3. Back to the Main menu.\n");
     printf("Command: ");
     scanf("%d" , &command);
     return command;
@@ -119,6 +133,12 @@ void List_ContactInfo(){
     }   //  List Empty
     int ID = 1;
     _info *Node = List;
+    printf("----------------------------------------------------------------------------\n");
+    printf("----------------------------------------------------------------------------\n");
+    if (ListLength == 1)
+        printf("There is only 1 contact here:\n");
+    else 
+        printf("There are %d contacts here:\n" , ListLength);
     while (Node){
         printf("****************************************************************************\n");
         printf("Contact ID:        %d\n" , ID ++);
@@ -135,6 +155,12 @@ void FILE_List_ContactInfo(FILE * stream , tm * GMTtime){
 
     int ID = 1;
     _info *Node = List;
+    fprintf(stream , "----------------------------------------------------------------------------\n");
+    fprintf(stream , "----------------------------------------------------------------------------\n");
+    if (ListLength == 1)
+        fprintf(stream , "There is only 1 contact here:\n");
+    else
+        fprintf(stream , "There are %d contacts here:\n" , ListLength);
     while (Node){
         fprintf(stream , "****************************************************************************\n");
         fprintf(stream , "Contact ID:        %d\n" , ID ++);
@@ -146,14 +172,17 @@ void FILE_List_ContactInfo(FILE * stream , tm * GMTtime){
 }   //FILE_List_ContactInfo
 
 void OutputToFile(bool isAutoSave){
-    if (ListLength == 0){
+    if (!isAutoSave && (ListLength == 0)){
         printf("****************************************************************************\n\n\n");
         Print_ErrorInfo(0);
         return ;
     }   //  List Empty
     FILE *FILEout;
-    if (isAutoSave)
+    if (isAutoSave){
+        if (ListLength > 0)
+            printf("\nCurrent contact list will auto save in the file \"ContactList_autosave.txt\"\n\n");
         FILEout = fopen(OUTPUT_FILE_AUTO , "w");
+    }
     else
         FILEout = fopen(OUTPUT_FILE , "w");
 
@@ -167,9 +196,11 @@ void OutputToFile(bool isAutoSave){
 
 int getDeleteChoice(){
     int command;
-    printf("\t\tHere are the commands you can type in (just type in the entry number)\n");
+    printf("--------------------------------------------------------------------------------\n");
+    printf("\t\tHere are the commands you can type in (just type in the entry number)\n\n");
     printf("\t\t1. Delete by name.\n");
     printf("\t\t2. Delete by telephone number.\n");
+    printf("\t\t3. Back to the Main menu.\n");
     printf("Command: ");
     scanf("%d" , &command);
     return command;
@@ -267,9 +298,11 @@ void Delete_Contact_bytelnum(){
 
 int getQueryChoice(){
     int command;
-    printf("\t\tHere are the commands you can type in (just type in the entry number)\n");
+    printf("--------------------------------------------------------------------------------\n");
+    printf("\t\tHere are the commands you can type in (just type in the entry number)\n\n");
     printf("\t\t1. Query by name.\n");
     printf("\t\t2. Query by telephone number.\n");
+    printf("\t\t3. Back to the Main menu.\n");
     printf("Command: ");
     scanf("%d" , &command);
     return command;
@@ -333,18 +366,90 @@ void Query_Contact_bytelnum(){
     }   //  Target does not exist
 }   //Query_Contact_bytelnum
 
+int getModifyChoice(int *TargetID_add){
+    int command;
+    char ch;
+    printf("--------------------------------------------------------------------------------\n");
+    printf("Attention: To use this function , you should first get the contact's ID. In order to get that , you can either use funtion 0 or 3 in the Main Menu. \'y\' to go ahead , \'n\' to return tu the Main Menu. \n");
+    scanf("%c%c" , &ch , &ch);
+    if (ch == 'n' || ch == 'N')
+        return -1;      //  Go back to the Main menu
+    printf("\t\tThe contact's ID: ");
+    scanf("%d" , TargetID_add);
+    printf("\t\tHere are the commands you can type in (just type in the entry number)\n\n");
+    printf("\t\t1. Modify the contact's name.\n");
+    printf("\t\t2. Modify the contact's telephone number.\n");
+    printf("\t\t3. Modify the contact's birthday date.\n");
+    printf("Command: ") ;
+    scanf("%d" , &command);
+    return command;
+}   //getModifyChoice
+
+void Modify_Contact_name(int TargetID){
+    int ID = 1;
+    char name[20];
+    _info *Node = List;
+    printf("\t\tThe new name: ");
+    scanf("%s" , name);
+
+    while (Node){
+        if (ID == TargetID){
+            memcpy(Node->name , name , sizeof(char) * 20);
+            break;
+        }   //  Find the target
+        ID ++;
+        Node = Node->next;
+    }   //while Node
+}   //Modify_Contact_name
+
+void Modify_Contact_telnum(int TargetID){
+    int ID = 1;
+    char telnum[20];
+    _info *Node = List;
+    printf("\t\tThe new telephone number: ");
+    scanf("%s" , telnum);
+
+    while (Node){
+        if (ID == TargetID){
+            memcpy(Node->telnum , telnum , sizeof(char) * 20);
+            break;
+        }   //  Find the target
+        ID ++;
+        Node = Node->next;
+    }   //while Node
+}   //Modify_Contact_telnum
+
+void Modify_Contact_birthday(int TargetID){
+    int ID = 1 , year , month , day;
+    _info *Node = List;
+    printf("\t\tThe new birthday date(**** ** **): ");
+    scanf("%d%d%d" , &year, &month , &day);
+    
+    while (Node){
+        if (ID == TargetID){
+            Node->birthday.year  = year;
+            Node->birthday.month = month;
+            Node->birthday.day   = day;
+            break;
+        }   //  Find the target
+        ID ++;
+        Node = Node->next;
+    }   //while Node
+}   //Modify_Contact_birthday
+
 int main(){
     Print_WelcomeInfo();
     while (true){
         Print_CommandInfo();
-        int command;
         scanf("%d" , &command);
         if (command == 0){
-            command = getListChoice();
-            if (command == 1)
+            int option = getListChoice();
+            if (option == 1)
                 List_ContactInfo();
-            else if (command == 2)
+            else if (option == 2)
                 OutputToFile(false);
+            else if (option == 3)
+                continue;
             else
                 Print_ErrorInfo(1);
         }   //  List
@@ -354,26 +459,45 @@ int main(){
         }   //  Add
 
         else if (command == 2){
-            command = getDeleteChoice();
-            if (command == 1)
+            int option = getDeleteChoice();
+            if (option == 1)
                 Delete_Contact_byname();
-            else if (command == 2)
+            else if (option == 2)
                 Delete_Contact_bytelnum();
+            else if (option == 3)
+                continue;
             else
                 Print_ErrorInfo(1);
         }   //  Delete
 
         else if (command == 3){
-            command = getQueryChoice();
-            if (command == 1)
+            int option = getQueryChoice();
+            if (option == 1)
                 Query_Contact_byname();
-            else if (command == 2)
+            else if (option == 2)
                 Query_Contact_bytelnum();
+            else if (option == 3)
+                continue;
             else
                 Print_ErrorInfo(1);
         }   //  Query
 
         else if (command == 4){
+            int TargetID;
+            int option = getModifyChoice(&TargetID);
+            if (option == -1)
+                continue;
+            else if (option == 1)
+                Modify_Contact_name(TargetID);
+            else if (option == 2)
+                Modify_Contact_telnum(TargetID);
+            else if (option == 3)
+                Modify_Contact_birthday(TargetID);
+            else
+                Print_ErrorInfo(1);
+        }   //  Modify
+
+        else if (command == 5){
             OutputToFile(true);
             return 0;
         }   //  Exit
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-This is the homework of Programming class. :(
-=============================================
+Welcome to use this contact gadget
+==================================
 
-It's just a gadget for contact.
--------------------------------
-Add a function that can save the contacts' information in a file named "ContactList.txt".
+
+Hello , this program is just my C language homework. Apart from the basic funtions , I add few functions and beautify the interface. So it would just look okay. :)
+
+---------
+
+If you have some more suggestions or ideas , or find some bugs in this program , you can contact me by the method below.
+
+* Telephone: (+86) 135 4139 1821
+* E-mail:	 Allen.3.ysj@gmail.com
+
+---------
+
+I'm happy you can enjoy this small gadget. :)
